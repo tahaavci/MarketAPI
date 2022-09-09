a68c143c0ae331e686a2acb1fabfc5ee1cf1ae69
tahaavci <110040025+tahaavci@users.noreply.github.com>
2022-09-10 02:32:51 +0300

    Update CommentServiceTest.java

--- src/test/java/com/market/api/Service/CommentServiceTest.java
+++ src/test/java/com/market/api/Service/CommentServiceTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CommentServiceTest{
 
